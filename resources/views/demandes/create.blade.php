00a079ef201dfd923f1f3d927e2b30aa10f0ef78
Blessing Nzemo <Anonymous.blessingnzemo243@gmail.com>
2024-06-11 03:13:23 +0100

    Ajout de la fonction de suppression

--- resources/views/demandes/create.blade.php
+++ resources/views/demandes/create.blade.php
@@ -108,16 +108,19 @@
                         <div>
                                 <x-input-label class="block mb-9 text-sm font-medium text-gray-900 dark:text-white"/>
 
-                                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Supprimer</a>
+                                <a href="#" class="delete-input text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Supprimer</a>
 
                                 </div>
 
                     </div>
                 `);
             });
-
+            // Ajout de la fonction de suppression
+            $(document).on('click', '.delete-input', function(e) {
+                e.preventDefault();
+                $(this).closest('.input-group').remove();
+            });
         });
-        
     </script>
 
 </x-app-layout>
